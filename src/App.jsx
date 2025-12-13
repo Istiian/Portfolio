53dea1c90ecd6ef65e09678ac24708e0bd68b6b4
Istian <markchristianforlaje@gmail.com>
2025-12-13 20:09:32 +0800

    Adding skill section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import NavigationBar from "./components/Navigation.jsx";
 import LandingSection from "./containers/LandingSection.jsx";
 import ProjectSection from "./containers/ProjectSection.jsx";
+import SkillsSection from "./containers/SkillsSection.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavigationBar />
       <LandingSection />
       <ProjectSection />
+      <SkillsSection/>
     </>
 
 
--- src/containers/SkillsSection.jsx
+++ src/containers/SkillsSection.jsx
@@ -1,18 +1,55 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import styling from '../styles/SkillsSection.module.css';
+
 
 export default function SkillsSection(){
     return(
-        <div>
+        <div className={styling.SkillsSection} id='skills'>
+            <h2 className={styling.sectionHeading}>Skills & Technologies</h2>
             <Container fluid="md">
-                <Row>
-                    <Col>Languages</Col>
-                    <Col>Database</Col>
+                <Row className={` mb-5`}>
+                    <Col>
+                        <h3 className={styling.skillHeadings}>Languages</h3>
+                        <div className={styling.skillsContainer}>
+                            <span className={styling.skills}>HTML</span>
+                            <span className={styling.skills}>CSS</span>
+                            <span className={styling.skills}>JavaScript</span>
+                            <span className={styling.skills}>PHP</span>
+                            <span className={styling.skills}>Python</span>
+                        </div>
+                        
+                    </Col>
+                    <Col>
+                        <h3 className={styling.skillHeadings}>Database</h3>
+                        <div className={styling.skillsContainer}>
+                            <span className={styling.skills}>MySQL</span>
+                        </div>
+                        
+                    </Col>
                 </Row>
                 <Row>
-                    <Col>Frameworks & Libraries</Col>
-                    <Col>Tools</Col>
+                    <Col>
+                        <h3 className={styling.skillHeadings}>Frameworks & Libraries</h3>
+                        <div className={styling.skillsContainer}>
+                            <span className={styling.skills}>ReactJS</span>
+                            <span className={styling.skills}>ExressJS</span>
+                            <span className={styling.skills}>Bootsrap</span>
+                        </div>
+                        
+                        
+                    </Col>
+
+                    <Col>
+                        <h3 className={styling.skillHeadings}>Tools</h3>
+                        <div className={styling.skillsContainer}>
+                            <span className={styling.skills}>Git</span>
+                            <span className={styling.skills}>Figma</span>
+                        </div>
+                        
+
+                    </Col>
                 </Row>
             </Container>
         </div>
