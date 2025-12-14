7b702a6ce84828803a6deea78a6a9daa105d3f0f
Istian <markchristianforlaje@gmail.com>
2025-12-14 16:42:03 +0800

    Fix myPic not found by vercel
    Fixes image import to resolve deployment error
    
    Updates the way the image is imported and referenced to ensure compatibility with Vercel's build system, preventing missing image errors on deployment.

--- src/containers/LandingSection.jsx
+++ src/containers/LandingSection.jsx
@@ -1,5 +1,6 @@
 import styling from "../styles/LandingSection.module.css";
 import Button from 'react-bootstrap/Button';
+import myPic from "../assets/Me.png"
 
 export default function LandingSection(){
     return(
@@ -13,7 +14,7 @@ export default function LandingSection(){
                 <Button onClick={()=>{window.open("https://github.com/Istiian", "_blank")}} variant="primary" size="lg" className={styling.GithubBtn}><i className="fa-brands fa-github" style={{color:"#2c2c2c"}}></i> Github</Button>
             </div>
             <div className={`col-12 col-lg-5 d-flex  justify-content-center`} style={{}}>
-                <img src="../src/assets/Me.png" alt="My Picture" className={`${styling.mypic} d-none d-lg-block`} />
+                <img src={myPic} alt="My Picture" className={`${styling.mypic} d-none d-lg-block`} />
             </div>
             
         </div>
