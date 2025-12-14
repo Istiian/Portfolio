de939c2c12d7bd2da7a1cca4e71eb8a0a0e13118
Istian <markchristianforlaje@gmail.com>
2025-12-14 16:57:59 +0800

    Refactor LandingSection styles for improved layout and responsiveness

--- src/containers/LandingSection.jsx
+++ src/containers/LandingSection.jsx
@@ -5,7 +5,7 @@ import myPic from "../assets/Me.png"
 export default function LandingSection(){
     return(
         <div className={`row ${styling.landingSection}`} id="landing">
-            <div className={`col-12 col-lg-7 px-auto`} style={{paddingTop:"100px"}}>
+            <div className={`col-12 col-lg-7 px-auto`} style={{}}>
                 <small  className={styling.greetings}>Welcome</small>
                 <h1 className={styling.header}>Mark Christian Forlaje</h1>
                 <p className={styling.desc}>I develop responsive web applications that offers innovative solutions. 
@@ -13,7 +13,7 @@ export default function LandingSection(){
                 
                 <Button onClick={()=>{window.open("https://github.com/Istiian", "_blank")}} variant="primary" size="lg" className={styling.GithubBtn}><i className="fa-brands fa-github" style={{color:"#2c2c2c"}}></i> Github</Button>
             </div>
-            <div className={`col-12 col-lg-5 d-flex  justify-content-center`} style={{}}>
+            <div className={`col-12 col-lg-5 d-flex  justify-content-center align-items-center`} style={{}}>
                 <img src={myPic} alt="My Picture" className={`${styling.mypic} d-none d-lg-block`} />
             </div>
             
